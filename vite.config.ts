0dda39241ca5679ef782c46810fb9605e2750279
PritamMajumder-MFSI <pritamm@mindfiresolutions.com>
2025-08-23 23:32:21 +0530

    change outdir

--- vite.config.ts
+++ vite.config.ts
@@ -13,9 +13,10 @@ export default defineConfig({
   },
   root: path.resolve(import.meta.dirname, "client"),
   build: {
-    outDir: path.resolve(import.meta.dirname, "dist/public"),
+    outDir: path.resolve(import.meta.dirname, "dist"),
     emptyOutDir: true,
   },
+
   server: {
     fs: {
       strict: true,
